7fc2c08a43673a1b7a2aded06bbc8bbc2c2c1139
PrietoAlejandro <faospitia2@misena.edu.co>
2022-10-05 14:06:26 -0500

    correccion responsive

--- js/hambu.js
+++ js/hambu.js
@@ -1,8 +1,23 @@
-'use strick'
-const icono = document.querySelector('.hamb')
+const menu = document.querySelector('.menu');
+const hamburger = document.querySelector('.hamburguer');
 
-const menu = document.querySelector('.menu')
+hamburger.onclick = () => 
+{
+    for (let i = 0; i < menu.classList.length; i++) 
+    {
 
-icono.addEventListener("click", ()=>{
-      menu.classList.toggle("active")
-})
+        const menuClass = menu.classList[i]
+
+        if (menuClass === "outside") 
+        {
+            menu.classList.remove('outside')
+            menu.classList.add('open')
+
+        } else if (menuClass === "open") 
+        {
+            menu.classList.remove('open')
+            menu.classList.add('outside')
+        }
+
+    }
+}
